787bfc8b116268297528e82e241f489e06317663
zxd <440922301@qq.com>
2016-03-16 23:55:39 +0800

    1

--- big_f2e_blog/routes/front_end/demo.js
+++ big_f2e_blog/routes/front_end/demo.js
@@ -1,13 +1,12 @@
 var express=require("express");
-
-var router=express.Router();
-
-var path=require("path");
-
-var static_dir="uploads/";
 var fs=require("fs");
-
+var path=require("path");
 var multer = require("multer");
+
+var images=require("images")
+var router=express.Router();
+var static_dir="uploads/";
+
 var upload = multer({ dest:static_dir})
 
 
@@ -19,9 +18,8 @@ router
 		res.render("front_end/demo.html",{
 			title:"图片上传demo"
 		})
-
 	})
-	.post("/demo",upload.single("recfile"),function(req,res){
+	.post("/demo",upload.single("avater"),function(req,res){
 		// var src=req.body.src;
 		// 
 		/**
@@ -32,22 +30,50 @@ router
 		 * 
 		 * maxsize 1048576
 		 */
+		
+		console.log(req.file);
 		if(req.file==undefined){
 			res.json({code:400,msg:"没有选中图片",srcOrigin:"",srcImg:"",srcIco:""});
+			fs.unlinkSync(req.file['path']);
 		}else{
 			if(req.file['size']>1048576){
-				res.json({"err":"文件过大，建议压缩后在传,建议 1M以内"});
+				res.json({code:400,"err":"文件过大，建议压缩后在传,建议 1M以内"});
+				fs.unlinkSync(req.file['path']);
 				return;
 			}
-
 			if(req.file.mimetype == "image/png" || req.file.mimetype == "image/gif" || req.file.mimetype == "image/jpeg" ){
 				var extname=path.extname(req.file['originalname'])
-				res.json({src:"1"});
+				var timstrap=Date.parse(new Date());
 				console.log(extname);
+
+				fs.readFile(req.file['path'],function(err,data){
+					if(err){
+						res.json({code:400,"err":"服务器读取失败"});
+
+					}else{
+						fs.writeFile(path.join(static_dir,"origin_"+timstrap+extname), data, function(err,data){
+							if(err){
+								res.json({code:400,"err":"服务器写入失败"});
+							}else{
+								res.json({code:200,src:"origin_"+timstrap+extname});
+								fs.unlinkSync(req.file['path']);
+								fs.unlinkSync(req.file['path']);	
+							}
+						});
+					}
+				});
+		
+				
+
+				// var imagedawp=images(req.file['path']).size().save();
+				// if(imagedawp){
+					
+				// }
 			}else{
-				res.json({err:"文件格式错误，请传 jpg / png / gif 图"});
+				res.json({code:400,err:"文件格式错误，请传 jpg / png / gif 图"});
 			}
-		}	
+		}
+
 	})
 
 
